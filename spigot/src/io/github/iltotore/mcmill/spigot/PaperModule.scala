ddd9455215580893e77209fab5bfd2799c06aeb0
Iltotore <rafbodaha@gmail.com>
2022-06-16 08:24:02 +0200

    fix: Missing Paper repository in PaperModule
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

--- spigot/src/io/github/iltotore/mcmill/spigot/PaperModule.scala
+++ spigot/src/io/github/iltotore/mcmill/spigot/PaperModule.scala
@@ -1,13 +1,21 @@
 package io.github.iltotore.mcmill.spigot
 
-import mill.Agg
-import mill.define.Target
+import coursier.Repository
+import coursier.maven.MavenRepository
+import mill.{Agg, T}
+import mill.define.{Target, Task}
 import mill.scalalib._
 
 /**
  * A module for Paper plugin development.
  */
 trait PaperModule extends SpigotModule {
+
+  override def repositoriesTask: Task[Seq[Repository]] = T.task {
+    super.repositoriesTask() ++ Seq(
+      MavenRepository("https://repo.papermc.io/repository/maven-public/")
+    )
+  }
 
   override def minecraftApiDeps: Target[Agg[Dep]] = Agg(
     if (formattedVersion.major >= 17) ivy"io.papermc.paper:paper-api:$spigotVersion"
